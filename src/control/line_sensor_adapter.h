0be85bd2ceb1077b4e6204ff96a8b044346d1e74
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-13 20:51:42 +0900

    ライン検出用センサの定義

---
+++ src/control/line_sensor_adapter.h
@@ -0,0 +1,19 @@
+#ifndef LINE_SENSOR_ADAPTER_H
+#define LINE_SENSOR_ADAPTER_H
+
+#include "sensor.h"
+#include "paper_color.h"
+
+/**
+ * ライン検出用センサー
+ */
+class LineSensorAdapter {
+    public:
+        LineSensorAdapter();
+        void Update();
+        PaperColor CurrentValue();
+    private:
+        Sensor sensor_;
+};
+
+#endif
