df88c6d7c1420fa52c9903953b6ce8b89607c84c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-14 01:06:39 +0900

    コードのフォーマット修正

--- src/control/wheel.h
+++ src/control/wheel.h
@@ -7,17 +7,17 @@
  * タイヤクラス
  */
 class Wheel {
-    public:
-        Wheel(int plus_pin, int minus_pin, int pwm_pin);
-        void UpdateSpeed(int speed, int milliseconds);
-        MoveDirection Direction();
-        int Speed();
-    private:
-        int plus_pin_;
-        int minus_pin_;
-        int pwm_pin_;
-        MoveDirection direction_ = forward;
-        int speed_;
+public:
+    Wheel(int plus_pin, int minus_pin, int pwm_pin);
+    void UpdateSpeed(int speed, int milliseconds);
+    MoveDirection Direction();
+    int Speed();
+private:
+    int plus_pin_;
+    int minus_pin_;
+    int pwm_pin_;
+    MoveDirection direction_ = forward;
+    int speed_;
 };
 
 #endif
