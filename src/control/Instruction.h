a98a5efd9de7c9d4598e30bd6c3900e6b8fb85a0
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-15 23:59:16 +0900

    Instructionの中身を実装（仮）

--- src/control/Instruction.h
+++ src/control/Instruction.h
@@ -3,7 +3,13 @@
 
 // 車に対する指令
 struct Instruction {
-    // TODO: 中身にどういうデータをもたせるか決める
+    int l_wheel_speed;      // 左車輪のスピード
+    int r_wheel_speed;      // 右車輪のスピード
+    MoveDirection l_wheel_direction;    // 左車輪の回転方向
+    MoveDirection r_wheel_direction;    // 右車輪の回転方向
+    int duration_millis;                // 処理にかける時間
+    bool enable_interpolation;          // 速度更新で補間を有効にするか
+    bool force;     // 強制実行するかどうか
 };
 
 #endif //INSTRUCTION_H
