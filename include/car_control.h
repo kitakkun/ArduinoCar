81863642e3604dfddf398987db9409eed325ba36
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-16 20:16:06 +0900

    ビルド通す用のファイル配置調整

---
+++ include/car_control.h
@@ -0,0 +1,15 @@
+#ifndef CAR_CONTROL_H
+#define CAR_CONTROL_H
+
+#include "../lib/car_control/brain.h"
+#include "../lib/car_control/car.h"
+#include "../lib/car_control/car_builder.h"
+#include "../lib/car_control/config.h"
+#include "../lib/car_control/instruction.h"
+#include "../lib/car_control/move_direction.h"
+#include "../lib/car_control/paper_color.h"
+#include "../lib/car_control/photo_reflector.h"
+#include "../lib/car_control/sensor.h"
+#include "../lib/car_control/wheel.h"
+
+#endif //CAR_CONTROL_H
