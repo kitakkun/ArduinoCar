633ee9ea87125cc2bce8ee791aaaca976f3b6d83
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-16 23:09:03 +0900

    photo_reflector.hにコメントを追加

--- lib/car_control/component/sensor/photo_reflector.h
+++ lib/car_control/component/sensor/photo_reflector.h
@@ -5,14 +5,25 @@
 #include "../../model/paper_color.h"
 
 /**
- * フォトリフレクタ（ライン検出用センサ）
+ * フォトリフレクタに対応するセンサークラス
  */
 class PhotoReflector : public Sensor {
 public:
+    /**
+     * コンストラクタ
+     * @param pin ピン番号
+     * @param theta 白黒の閾値
+     */
     PhotoReflector(int pin, int theta);
+
+    /**
+     * センサーが認識した色を取得
+     * @return 認識した色（白・黒）
+     */
     PaperColor Value() const;
+
 private:
-    int theta_;         // 白か黒か判定する境目
+    int theta_;      /// 白か黒か判定する閾値
 };
 
 #endif
