92dfcfc43fdf920b461c98911224d59299b2c1a0
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-16 23:07:05 +0900

    sensor.hにコメントを追加

--- lib/car_control/component/sensor/sensor.h
+++ lib/car_control/component/sensor/sensor.h
@@ -2,16 +2,31 @@
 #define SENSOR_DATA_H
 
 /**
- * センサークラス（センサーの値更新・提供）
+ * センサークラス
+ * センサー値の更新・保持・提供を行う
  */
 class Sensor {
 public:
+    /**
+     * コンストラクタ
+     * @param pin ピン番号
+     */
     explicit Sensor(int pin);
+
+    /**
+     * センサー値を更新
+     */
     void Update();
+
+    /**
+     * センサー値の取得（生のデータ）
+     * @return 生のセンサー値
+     */
     int RawValue() const;
+
 private:
-    int pin_;
-    int value_;
+    int pin_;       /// センサーのピン番号
+    int value_;     /// センサーの値
 };
 
 #endif
