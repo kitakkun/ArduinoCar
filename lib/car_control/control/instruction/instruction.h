d0998c0aaad4db198c1a8c9ee3fa4d3efbf87485
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-16 23:01:51 +0900

    instruction.hにコメント追加・割り込みの仕様変更

--- lib/car_control/control/instruction/instruction.h
+++ lib/car_control/control/instruction/instruction.h
@@ -3,14 +3,29 @@
 
 #include "../../component/wheel/wheel.h"
 
-// 車に対する指令
+/**
+ * 車に対する指令を表す抽象クラス
+ * 継承して中身を実装することで様々な制御を作れる
+ */
 class Instruction {
 public:
+    /**
+     * デストラクタ
+     */
     virtual ~Instruction() = default;
+
+    /**
+     * 車の車輪を制御する。
+     * @param left_wheel 左車輪のインスタンス
+     * @param right_wheel 右車輪のインスタンス
+     */
     virtual void Run(Wheel* left_wheel, Wheel* right_wheel) = 0;
-    bool InterruptionEnabled();
-private:
-    bool enable_interruption = false;   // 割り込み処理するかどうかのフラグ
+
+    /**
+     * 割り込みをするか否かを取得。
+     * @return 割り込みが有効かどうか
+     */
+    virtual bool InterruptionEnabled() = 0;
 };
 
 #endif //INSTRUCTION_H
