df5fe46a41c8e8781bbae3e5190201c9e1405645
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-16 22:43:00 +0900

    car.hにコメントを書いた

--- lib/car_control/car/car.h
+++ lib/car_control/car/car.h
@@ -11,20 +11,49 @@ class CarBuilder;   // 循環参照防止
 
 class Car {
 public:
-    explicit Car(CarBuilder *builder);  // コンストラクタ（Builderを使って作る）
-    void UpdateSensors();           // センサー情報の更新
-    CarState CollectCarState();     // センサー情報や車輪の回転情報など車に関する情報を取得
-    Instruction *Think(CarState state);  // 次に取るべき行動を決定する
+    /**
+     * builderのパラメータを元にインスタンスを生成します。
+     * @param builder CarBuilderのインスタンス
+     */
+    explicit Car(CarBuilder *builder);
+
+    /**
+     * センサー情報を更新します。
+     */
+    void UpdateSensors();
+
+    /**
+     * センサー情報や車輪の回転情報など車に関する情報を返す。
+     * @return 車の状態情報
+     */
+    CarState CollectCarState();
+
+    /**
+     * 次に取るべき行動を決定する（Brainクラスが決定した指令が返る）。
+     * @param state 判断材料となる車の状態情報
+     * @return 取るべきだと判断した行動
+     */
+    Instruction *Think(CarState state);
+
+    /**
+     * 指令をセットします。
+     * @param instruction セットする指令
+     */
     void SetInstruction(Instruction *instruction);
-    void Act();  // 決定した行動を実行に写す
+
+    /**
+     * 設定されている指令を実行に移す。
+     */
+    void Act();
+
 private:
-    Instruction *instruction_;   // 実行中の指令
-    Brain *brain_;           // 行動決定（アルゴリズム実装クラス）
-    Wheel *left_wheel_;     // 左の車輪
-    Wheel *right_wheel_;    // 右の車輪
-    PhotoReflector *left_reflector_;    // 左のセンサ
-    PhotoReflector *right_reflector_;   // 右のセンサ
-    PhotoReflector *front_reflector_;   // 真ん中のセンサ
+    Instruction *instruction_;   /// 設定中の指令
+    Brain *brain_;          /// 車の状態を見て指令を送るアルゴリズム実装部
+    Wheel *left_wheel_;     /// 左の車輪
+    Wheel *right_wheel_;    /// 右の車輪
+    PhotoReflector *left_reflector_;    /// 左のセンサ
+    PhotoReflector *right_reflector_;   /// 右のセンサ
+    PhotoReflector *front_reflector_;   /// 真ん中のセンサ
 };
 
 #endif
