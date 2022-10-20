767574c9cc9586f4bd328bf337f672b46a5988a9
unchang <sagumemurata31@gmail.com>
2022-10-20 16:12:20 +0900

    min_speed_とmx_speed_にthisを加えた

--- lib/car_control/control/stabilizer/wheel_stablizer.cpp
+++ lib/car_control/control/stabilizer/wheel_stablizer.cpp
@@ -1,10 +1,10 @@
 # include "wheel_stabilizer.h"
 
 void WheelStablizer::Stablize(Wheel *left_wheel,Wheel *right_wheel){
-    if(left_wheel->Speed() < min_speed_) left_wheel->UpdateSpeed(min_speed_);
-    if(left_wheel->Speed() > max_speed_) left_wheel->UpdateSpeed(max_speed_);
-    if(right_wheel->Speed() < min_speed_) right_wheel->UpdateSpeed(min_speed_);
-    if(right_wheel->Speed() > max_speed_) right_wheel->UpdateSpeed(max_speed_);     
+    if(left_wheel->Speed() < this->min_speed_) left_wheel->UpdateSpeed(this->min_speed_);
+    if(left_wheel->Speed() > this->max_speed_) left_wheel->UpdateSpeed(this->max_speed_);
+    if(right_wheel->Speed() < this->min_speed_) right_wheel->UpdateSpeed(this->min_speed_);
+    if(right_wheel->Speed() > this->max_speed_) right_wheel->UpdateSpeed(this->max_speed_);     
 }
 
 WheelStabilizer::WheelStabilizer(int min_speed, int max_speed){
