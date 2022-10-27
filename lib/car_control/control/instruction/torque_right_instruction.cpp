2f2c2e78c272581e0009efdd73a4ad81b877e97e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-27 14:02:36 +0900

    右折・左折指令の修正

--- lib/car_control/control/instruction/torque_right_instruction.cpp
+++ lib/car_control/control/instruction/torque_right_instruction.cpp
@@ -1,7 +1,7 @@
 #include "torque_right_instruction.h"
 
 void TorqueRightInstruction::Run(Wheel *left_wheel, Wheel *right_wheel) {
-    left_wheel->UpdateSpeed(right_wheel->Speed() - this->force_);
+    right_wheel->UpdateSpeed(left_wheel->Speed() - this->force_);
 }
 
 TorqueRightInstruction::TorqueRightInstruction(int force) {
