42299c72fa69126b3c1431855f14558fa6cf09d1
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-10-27 13:25:08 +0900

    アルゴリズムの変更。軽微なエラー修正。

--- lib/car_control/control/brain/zigzag_line_trace_brain.cpp
+++ lib/car_control/control/brain/zigzag_line_trace_brain.cpp
@@ -2,16 +2,31 @@
 #include "../instruction/torque_left_instruction.h"
 #include "../instruction/torque_right_instruction.h"
 #include "../instruction/force_speed_update_instruction.h"
+#include "control/instruction/empty_instruction.h"
+#include "control/instruction/force_stop_instruction.h"
 
 Instruction *ZigZagLineTraceBrain::CalculateNextInstruction(CarState state) {
-    if (state.lw_speed == 0 && state.rw_speed == 0) {
-        return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-    } else if (state.left_color == white) {
-        return new TorqueRightInstruction(torque_force_);
-    } else if (state.right_color == white){
-        return new TorqueLeftInstruction(torque_force_);
+    if (this->state_ == READY) {
+        this->state_ = TRACING_LINE;
+        return new EmptyInstruction();
     }
-    return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
+    if (this->state_ == TRACING_LINE) {
+        if (state.lw_speed == 0 && state.rw_speed == 0) {
+            return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
+        }
+        // ライン上にいる
+        if (state.front_color == black) {
+            if (state.left_color == black) {
+                return new TorqueLeftInstruction(torque_force_);
+            } else if (state.right_color == black) {
+                return new TorqueRightInstruction(torque_force_);
+            }
+            return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
+        }
+        this->state_ = FINISHED;
+        return new ForceStopInstruction();
+    }
+    return new EmptyInstruction();
 }
 
 ZigZagLineTraceBrain::ZigZagLineTraceBrain(int run_speed, int torque_force) {
--- lib/car_control/control/brain/zigzag_line_trace_brain.h
+++ lib/car_control/control/brain/zigzag_line_trace_brain.h
@@ -3,14 +3,20 @@
 
 #include "brain.h"
 
+enum ZigZagLineTraceState {
+    READY, TRACING_LINE, FINISHED
+};
+
 class ZigZagLineTraceBrain : public Brain {
 public:
     ZigZagLineTraceBrain(int run_speed, int torque_force);
 
     Instruction *CalculateNextInstruction(CarState state) override;
+
 private:
     int run_speed_;
     int torque_force_;
+    ZigZagLineTraceState state_ = READY;
 };
 
 #endif // ZIGZAG_LINE_TRACE_BRAIN
