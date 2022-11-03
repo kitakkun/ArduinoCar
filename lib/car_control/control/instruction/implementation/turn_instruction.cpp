070152d48e6d243f0cdfdb93c24a3d5e31b1fdf4
Fujita723 <115635408+Fujita723@users.noreply.github.com>
2022-11-03 21:31:24 +0900

    回転する指令を修正しました

--- lib/car_control/control/instruction/implementation/turn_instruction.cpp
+++ lib/car_control/control/instruction/implementation/turn_instruction.cpp
@@ -4,8 +4,10 @@ void TurnInstruction::Run(Wheel *left_wheel, Wheel *right_wheel) {
     left_wheel->UpdateDirection(backward); /// 左車輪を後ろ方向に回転させる
     left_wheel->UpdateSpeed(100 /* 任意の速さ */);
 
-    right_wheel->UpdateSpeed(100 /* 任意の速さ */); /// 右車輪を前方向に回転させる
+    right_wheel->UpdateDirection(forward); /// 右車輪を前方向に回転させる
+    right_wheel->UpdateSpeed(100 /* 任意の速さ */);
 }
 
-/* 走行時間を指定し，180度回転するようにする */
+/* TODO: 走行時間を指定し，180度回転するようにする */
+/* TODO: 回転前の状態(Direction)を記憶し，回転後に，回転前の状態へ戻す */
 
