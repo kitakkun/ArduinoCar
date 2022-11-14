bd5e58dd321860de1d325a76ef3207082fdc4bb1
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-11-14 13:39:38 +0900

    dummy_brainの削除

--- lib/car_control/custom/brain/dummy_brain.cpp
+++
@@ -1,7 +0,0 @@
-#include "dummy_brain.h"
-#include "custom/instruction/log_instruction.h"
-#include "custom/instruction/turn_instruction.h"
-
-Instruction *DummyBrain::CalculateNextInstruction(CarState state) {
-    return new TurnInstruction(100, 2000);
-}
