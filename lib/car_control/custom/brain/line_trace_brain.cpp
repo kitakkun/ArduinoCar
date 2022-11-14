034d9f36bef9807a1b982e76a32bf12738ab1b86
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-11-14 22:24:35 +0900

    ライントレース周りの整理と致命的なバグ修正
    brainがnullptrになってバグって落ちてたのを直した

--- lib/car_control/custom/brain/line_trace_brain.cpp
+++ lib/car_control/custom/brain/line_trace_brain.cpp
@@ -1,152 +1,146 @@
 #include "line_trace_brain.h"
-#include "custom/instruction/torque_left_instruction.h"
-#include "custom/instruction/torque_right_instruction.h"
-#include "custom/instruction/force_speed_update_instruction.h"
-#include "custom/instruction/force_stop_instruction.h"
-#include "custom/instruction/wait_instruction.h"
+#include "custom/instructions.h"
 #include "ArduinoLog.h"
-#include "custom/instruction/update_direction_instruction.h"
-#include "custom/instruction/delta_speed_update_instruction.h"
 #include "core/data_model/binary_color.h"
+#include "custom/debug/logger.h"
 
-#define THRESHOLD 10
-
-int calc_weight(int prev, int current) {
-    int value = current - prev;
-    if (value > THRESHOLD) {
-        value = THRESHOLD;
-    } else if (value < -THRESHOLD) {
-        value = -THRESHOLD;
-    }
-    return value;
-}
-
-Instruction *LineTraceBrain::CalculateNextInstruction() {
-    // 準備中
-    if (this->state_ == READY) {
-        // 発進してライン探索モードに移行
-        this->state_ = SEARCHING_LINE;
-        return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-    }
-    // ライン探索中
-    if (this->state_ == SEARCHING_LINE) {
-        // どれか一つのセンサーが黒になったらトレース開始・一旦停止
-        if (current_car_state_.front_left_reflector_color == black || current_car_state_.front_mid_reflector_color == black ||
-            current_car_state_.front_right_reflector_color == black) {
-            trace_start_time_ = millis();
-            state_ = TRACING_LINE;
-            return new ForceStopInstruction();
-        }
-        return new WaitInstruction(10);
-    }
-    // ライントレース中
-    if (this->state_ == TRACING_LINE) {
-        unsigned long current_time = millis();
-
-        // スピードが0なら発進
-        if (current_car_state_.left_wheel_speed == 0 || current_car_state_.right_wheel_speed == 0) {
-            return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-        }
-
-        // 全部黒のとき
-//        if (current_car_state_.front_mid_reflector_color == black && current_car_state_.front_left_reflector_color == black &&
-//            current_car_state_.front_right_reflector_color == black) {
-//            return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-//        }
-
-        // 全部白のとき
-        if (current_car_state_.front_mid_reflector_color == white && current_car_state_.front_left_reflector_color == white &&
-            current_car_state_.front_right_reflector_color == white) {
-            if (current_time - trace_start_time_ > 1500) {
-                state_ = READY_FOR_BACK;
-                return new ForceStopInstruction();
-            }
-            return new DeltaSpeedUpdateInstruction(-5);
-        }
-
-        // 左が黒なら左へ曲がる
-        if (current_car_state_.front_left_reflector_color == black) {
-            trace_start_time_ = millis();
-            return new TorqueLeftInstruction(torque_force_, 30);
-        }
-        // 右が黒なら右へ曲がる
-        if (current_car_state_.front_right_reflector_color == black) {
-            trace_start_time_ = millis();
-            return new TorqueRightInstruction(torque_force_, 30);
-        }
-        // 真ん中が黒なら直進
-        if (current_car_state_.front_mid_reflector_color == black) {
-            trace_start_time_ = millis();
-            return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-        }
-
-        return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-    }
-    if (this->state_ == READY_FOR_BACK) {
-        trace_start_time_ = millis();
-        this->state_ = SEARCHING_BACK_LINE;
-        return new UpdateDirectionInstruction(backward);
-    }
-    if (this->state_ == SEARCHING_BACK_LINE) {
-        if (current_car_state_.front_left_reflector_color == black || current_car_state_.front_mid_reflector_color == black ||
-            current_car_state_.front_right_reflector_color == black) {
-            trace_start_time_ = millis();
-            state_ = TRACING_BACK_LINE;
-            return new ForceStopInstruction();
-        }
-        return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-    }
-    if (this->state_ == TRACING_BACK_LINE) {
-
-        unsigned long current_time = millis();
-        if (current_car_state_.left_wheel_speed == 0 || current_car_state_.right_wheel_speed == 0) {
-            return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-        }
-
-        if (current_car_state_.front_mid_reflector_color == white && current_car_state_.front_left_reflector_color == white &&
-            current_car_state_.front_right_reflector_color == white) {
-            if (current_time - trace_start_time_ > 1000) {
-                state_ = FINISHED;
-                return new ForceStopInstruction();
-            } else {
-                if (last_instruction_ == TorqueRight) {
-                    last_instruction_ = TorqueRight;
-                    return new TorqueRightInstruction(torque_force_, 30);
-                } else if (last_instruction_ == TorqueLeft) {
-                    last_instruction_ = TorqueLeft;
-                    return new TorqueLeftInstruction(torque_force_, 30);
-                }
-                return new DeltaSpeedUpdateInstruction(-5);
-            }
-        }
-
-        last_instruction_ = Other;
-        if (current_car_state_.front_right_reflector_color == black) {
-            trace_start_time_ = millis();
-            last_instruction_ = TorqueRight;
-            return new TorqueRightInstruction(torque_force_ - 15, 30);
-        }
-        if (current_car_state_.front_left_reflector_color == black) {
-            trace_start_time_ = millis();
-            last_instruction_ = TorqueLeft;
-            return new TorqueLeftInstruction(torque_force_ - 15, 30);
-        }
-        if (current_car_state_.front_mid_reflector_color == black) {
-            trace_start_time_ = millis();
-            return new ForceSpeedUpdateInstruction(run_speed_ - 15, run_speed_);
-        }
-
-        return new ForceSpeedUpdateInstruction(run_speed_, run_speed_);
-    }
-    return new ForceStopInstruction();
-
-}
-
-LineTraceBrain::LineTraceBrain(int run_speed, int torque_force) {
-    this->run_speed_ = run_speed;
+LineTraceBrain::LineTraceBrain(int base_speed, int torque_force) {
+    this->base_speed_ = base_speed;
     this->torque_force_ = torque_force;
 }
 
 void LineTraceBrain::SetCurrentCarState(LineTraceCarState car_state) {
-    current_car_state_  = car_state;
+    Logger::LogLineTraceCarState(car_state);
+    current_car_state_ = car_state;
+}
+
+Instruction *LineTraceBrain::CalculateNextInstruction() {
+    switch (state_) {
+        case ready:
+            return Ready();
+        case searching:
+            return Search();
+        case tracing:
+            return Trace();
+        case readyBack:
+            return ReadyBack();
+        case searchingBack:
+            return SearchBack();
+        case tracingBack:
+            return TraceBack();
+        default:
+            return new ForceStopInstruction();
+    }
+}
+
+// 準備中（行き）
+Instruction *LineTraceBrain::Ready() {
+    // 発進してライン探索モードに移行
+    this->state_ = searching;
+    return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
+}
+
+// 黒線探索モード（行き）
+Instruction *LineTraceBrain::Search() {
+    // どれか一つのセンサーが黒になったらトレース開始・一旦停止
+    if (current_car_state_.IsAnyFrontBlack()) {
+        last_time_on_black_ = millis();
+        state_ = tracing;
+        return new ForceStopInstruction();
+    }
+    return new WaitInstruction(10);
+}
+
+// トレース中（行き）
+Instruction *LineTraceBrain::Trace() {
+
+    if (current_car_state_.IsAnyFrontBlack()) {
+        last_time_on_black_ = millis();
+    }
+
+    unsigned long current_time = millis();
+
+    // スピードが0なら発進
+    if (current_car_state_.left_wheel_speed_ == 0 || current_car_state_.right_wheel_speed_ == 0) {
+        return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
+    }
+
+    // 全部白で一定時間経過したらトレース完了とみなしバックモードへ（一旦停止）
+    if (current_car_state_.IsAllFrontWhite() && current_time - last_time_on_black_ > 1500) {
+        state_ = readyBack;
+        return new ForceStopInstruction();
+    }
+
+    // 左が黒なら左へ曲がる
+    if (current_car_state_.front_left_reflector_color_ == black) {
+        return new TorqueLeftInstruction(torque_force_, 30);
+    }
+    // 右が黒なら右へ曲がる
+    if (current_car_state_.front_right_reflector_color_ == black) {
+        return new TorqueRightInstruction(torque_force_, 30);
+    }
+    // 真ん中が黒なら直進
+    if (current_car_state_.front_mid_reflector_color_ == black) {
+        return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
+    }
+
+    return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
+}
+
+Instruction *LineTraceBrain::ReadyBack() {
+    last_time_on_black_ = millis();
+    this->state_ = searchingBack;
+    return new UpdateDirectionInstruction(backward);
+}
+
+Instruction *LineTraceBrain::SearchBack() {
+    if (current_car_state_.IsAnyFrontBlack()) {
+        last_time_on_black_ = millis();
+        state_ = tracingBack;
+        return new ForceStopInstruction();
+    }
+    return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
+}
+
+Instruction *LineTraceBrain::TraceBack() {
+    unsigned long current_time = millis();
+    if (current_car_state_.left_wheel_speed_ == 0 || current_car_state_.right_wheel_speed_ == 0) {
+        return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
+    }
+
+    if (current_car_state_.front_mid_reflector_color_ == white &&
+        current_car_state_.front_left_reflector_color_ == white &&
+        current_car_state_.front_right_reflector_color_ == white) {
+        if (current_time - last_time_on_black_ > 1000) {
+            state_ = FINISHED;
+            return new ForceStopInstruction();
+        } else {
+            if (last_instruction_ == TorqueRight) {
+                last_instruction_ = TorqueRight;
+                return new TorqueRightInstruction(torque_force_, 30);
+            } else if (last_instruction_ == TorqueLeft) {
+                last_instruction_ = TorqueLeft;
+                return new TorqueLeftInstruction(torque_force_, 30);
+            }
+            return new DeltaSpeedUpdateInstruction(-5);
+        }
+    }
+
+    last_instruction_ = Other;
+    if (current_car_state_.front_right_reflector_color_ == black) {
+        last_time_on_black_ = millis();
+        last_instruction_ = TorqueRight;
+        return new TorqueRightInstruction(torque_force_ - 15, 30);
+    }
+    if (current_car_state_.front_left_reflector_color_ == black) {
+        last_time_on_black_ = millis();
+        last_instruction_ = TorqueLeft;
+        return new TorqueLeftInstruction(torque_force_ - 15, 30);
+    }
+    if (current_car_state_.front_mid_reflector_color_ == black) {
+        last_time_on_black_ = millis();
+        return new ForceSpeedUpdateInstruction(base_speed_ - 15, base_speed_);
+    }
+
+    return new ForceSpeedUpdateInstruction(base_speed_, base_speed_);
 }
--- lib/car_control/custom/data_model/line_trace_car_state.h
+++ lib/car_control/custom/data_model/line_trace_car_state.h
@@ -3,24 +3,72 @@
 
 #include "core/data_model/binary_color.h"
 #include "core/data_model/move_direction.h"
+#include "core/component/wheel.h"
+#include "core/component/photo_reflector.h"
 
-struct LineTraceCarState {
-    int left_wheel_speed;           // 左車輪のスピード
-    int right_wheel_speed;           // 右車輪のスピード
-    MoveDirection left_wheel_direction; // 左車輪の回転方向
-    MoveDirection right_wheel_direction; // 右車輪の回転方向
-    BinaryColor front_mid_reflector_color;     // 中央センサーの色
-    BinaryColor front_left_reflector_color;      // 左センサーの色
-    BinaryColor front_right_reflector_color;     // 右センサーの色
-    BinaryColor back_mid_reflector_color;     // 中央センサーの色
-    BinaryColor back_left_reflector_color;      // 左センサーの色
-    BinaryColor back_right_reflector_color;     // 右センサーの色
-    int front_mid_reflector_raw;      // 中央センサーの生の値
-    int front_left_reflector_raw;       // 左センサーの生の値
-    int front_right_reflector_raw;      // 右センサーの生の値
-    int back_mid_reflector_raw;      // 中央センサーの生の値
-    int back_left_reflector_raw;       // 左センサーの生の値
-    int back_right_reflector_raw;      // 右センサーの生の値
+class LineTraceCarState {
+public:
+    LineTraceCarState();
+
+    LineTraceCarState(
+            int left_wheel_speed,
+            int right_wheel_speed,
+            MoveDirection left_wheel_direction,
+            MoveDirection right_wheel_direction,
+            BinaryColor front_mid_reflector_color,
+            BinaryColor front_left_reflector_color,
+            BinaryColor front_right_reflector_color,
+            BinaryColor back_mid_reflector_color,
+            BinaryColor back_left_reflector_color,
+            BinaryColor back_right_reflector_color,
+            int front_mid_reflector_raw,
+            int front_left_reflector_raw,
+            int front_right_reflector_raw,
+            int back_mid_reflector_raw,
+            int back_left_reflector_raw,
+            int back_right_reflector_raw
+    );
+
+    bool IsAnyBlack() const;
+
+    bool IsAnyFrontBlack() const;
+
+    bool IsAnyBackBlack() const;
+
+    bool IsAnyWhite() const;
+
+    bool IsAnyFrontWhite() const;
+
+    bool IsAnyBackWhite() const;
+
+    bool IsAllWhite() const;
+
+    bool IsAllFrontWhite() const;
+
+    bool IsAllBackWhite() const;
+
+    bool IsAllBlack() const;
+
+    bool IsAllFrontBlack() const;
+
+    bool IsAllBackBlack() const;
+
+    int left_wheel_speed_;           // 左車輪のスピード
+    int right_wheel_speed_;           // 右車輪のスピード
+    MoveDirection left_wheel_direction_; // 左車輪の回転方向
+    MoveDirection right_wheel_direction_; // 右車輪の回転方向
+    BinaryColor front_mid_reflector_color_;     // 中央センサーの色
+    BinaryColor front_left_reflector_color_;      // 左センサーの色
+    BinaryColor front_right_reflector_color_;     // 右センサーの色
+    BinaryColor back_mid_reflector_color_;     // 中央センサーの色
+    BinaryColor back_left_reflector_color_;      // 左センサーの色
+    BinaryColor back_right_reflector_color_;     // 右センサーの色
+    int front_mid_reflector_raw_;      // 中央センサーの生の値
+    int front_left_reflector_raw_;       // 左センサーの生の値
+    int front_right_reflector_raw_;      // 右センサーの生の値
+    int back_mid_reflector_raw_;      // 中央センサーの生の値
+    int back_left_reflector_raw_;       // 左センサーの生の値
+    int back_right_reflector_raw_;      // 右センサーの生の値
 };
 
 #endif //LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_LINE_TRACE_CAR_STATE_H
