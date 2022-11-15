e19f57e894b8ba2701885db986d5c488dc10e44e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-11-15 12:10:20 +0900

    Instructionのコンストラクタにmode指定を追加

--- lib/car_control/core/logic/instruction.cpp
+++ lib/car_control/core/logic/instruction.cpp
@@ -1,5 +1,11 @@
 #include "instruction.h"
 
+Instruction::Instruction(InstructionMode mode) {
+    this->mode_ = mode;
+    this->left_wheel_ = nullptr;
+    this->right_wheel_ = nullptr;
+}
+
 void Instruction::Setup(Wheel *left_wheel, Wheel *right_wheel) {
     this->left_wheel_ = left_wheel;
     this->right_wheel_ = right_wheel;
