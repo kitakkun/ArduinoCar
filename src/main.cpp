bd330b3966b7362a8b61da0aa13010bb5de7a463
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-11-24 00:24:15 +0900

    Configファイルのマクロ名修正

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,9 @@ void setup() {
             .SetBrain(new LineTraceGoAndBackBrain(BASE_SPEED, FORWARD_TORQUE, BACKWARD_TORQUE))
             .SetLeftWheel(new WheelImpl(LEFT_MOTOR_PLUS_PIN, LEFT_MOTOR_MINUS_PIN, LEFT_MOTOR_PWM_PIN, "LeftWheel"))
             .SetRightWheel(new WheelImpl(RIGHT_MOTOR_PLUS_PIN, RIGHT_MOTOR_MINUS_PIN, RIGHT_MOTOR_PWM_PIN, "RightWheel"))
-            .SetMidReflector(new PhotoReflectorImpl(FRONT_MID_PHOTO_REFLECTOR_PIN, PHOTO_REFLECTOR_THRESHOLD, "Mid"))
-            .SetRightReflector(new PhotoReflectorImpl(FRONT_RIGHT_PHOTO_REFLECTOR_PIN, PHOTO_REFLECTOR_THRESHOLD, "Right"))
-            .SetLeftReflector(new PhotoReflectorImpl(FRONT_LEFT_PHOTO_REFLECTOR_PIN, PHOTO_REFLECTOR_THRESHOLD, "Left"))
+            .SetMidReflector(new PhotoReflectorImpl(MID_PHOTO_REFLECTOR_PIN, PHOTO_REFLECTOR_THRESHOLD, "Mid"))
+            .SetRightReflector(new PhotoReflectorImpl(RIGHT_PHOTO_REFLECTOR_PIN, PHOTO_REFLECTOR_THRESHOLD, "Right"))
+            .SetLeftReflector(new PhotoReflectorImpl(LEFT_PHOTO_REFLECTOR_PIN, PHOTO_REFLECTOR_THRESHOLD, "Left"))
             .Build();
     Log.verboseln("Done! Started Program Lifecycle.");
 
