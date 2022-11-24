67631aa1506128c721dca6caf8bd04c4d1784f84
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-11-24 10:40:53 +0900

    trace_modeによる制御分岐を追加

--- lib/car_control/custom/brain/pid_line_trace_brain.cpp
+++ lib/car_control/custom/brain/pid_line_trace_brain.cpp
@@ -1,6 +1,16 @@
 #include "pid_line_trace_brain.h"
 #include "custom/instructions.h"
 
+PidLineTraceBrain::PidLineTraceBrain(TraceMode trace_mode, int base_speed, int turn_speed, float p, float d, int lr_sensor_diff, int max_manipulation) {
+    this->trace_mode_ = trace_mode;
+    this->base_speed_ = base_speed;
+    this->turn_speed_ = turn_speed;
+    this->p_ = p;
+    this->d_ = d;
+    this->lr_sensor_diff_ = lr_sensor_diff;
+    this->max_manipulation_ = max_manipulation;
+}
+
 Instruction *PidLineTraceBrain::Ready() {
     this->activity_state_ = searching;
     last_time_called_ = millis();
@@ -18,8 +28,8 @@ Instruction *PidLineTraceBrain::Search() {
 
 Instruction *PidLineTraceBrain::Trace() {
 
-    // 全部白になったらトレース完了
-    if (current_car_state_.IsAllWhite()) {
+    // 全部白になったらトレース完了(継続トレースモードのときは完了しない）
+    if (current_car_state_.IsAllWhite() && this->trace_mode_ != continuous) {
         this->activity_state_ = readyBack;
         return new UpdateSpeedInstruction(0);
     }
@@ -36,12 +46,12 @@ Instruction *PidLineTraceBrain::Trace() {
      * 計算された操作量を用いて微分制御を行う（D制御）
      */
     unsigned long delta_time = millis() - last_time_called_;
-    float deviation_differential = (float)(deviation - prev_deviation_) / (float)delta_time;
+    float deviation_differential = (float) (deviation - prev_deviation_) / (float) delta_time;
 
     /**
      * 操作量の計算（PIDの各重みを考慮して最終的な操作量を計算する）
      */
-    int manipulation = (int)(p_ * deviation + d_ * deviation_differential);
+    int manipulation = (int) (p_ * deviation + d_ * deviation_differential);
 
     // 操作量の範囲を制限
     manipulation = constrain(manipulation, -max_manipulation_, max_manipulation_);
@@ -49,22 +59,36 @@ Instruction *PidLineTraceBrain::Trace() {
     // deviationの上書きと時間更新（次回の準備）
     prev_deviation_ = deviation;
     last_time_called_ = millis();
-    
+
     return new UpdateSpeedInstruction(base_speed_ - manipulation, base_speed_ + manipulation);
 }
 
 Instruction *PidLineTraceBrain::ReadyBack() {
     // タイヤの回転方向を切り替える
     this->activity_state_ = searchingBack;
-    return new UpdateDirectionInstruction(forward);
+    if (this->trace_mode_ == goBack) {
+        // 前進・後退ならタイヤの回転方向を切り替える
+        return new UpdateDirectionInstruction(forward);
+    } else {
+        // Uターンモードなら回転命令を出す
+        return new TurnInstruction(turn_speed_, 100);
+    }
 }
 
 Instruction *PidLineTraceBrain::SearchBack() {
-    if (this->current_car_state_.IsAnyBlack()) {
-        this->activity_state_ = tracingBack;
-        return new UpdateSpeedInstruction(0);
+    if (this->trace_mode_ == goBack) {
+        if (this->current_car_state_.IsAnyBlack()) {
+            this->activity_state_ = tracingBack;
+            return new UpdateSpeedInstruction(0);
+        } else {
+            return new UpdateSpeedInstruction(base_speed_);
+        }
     } else {
-        return new UpdateSpeedInstruction(base_speed_);
+        if (this->current_car_state_.mid_reflector_color_ == black) {
+            this->activity_state_ = tracingBack;
+            return new UpdateSpeedInstruction(0);
+        }
+        return new TurnInstruction(turn_speed_, 100);
     }
 }
 
@@ -90,12 +114,12 @@ Instruction *PidLineTraceBrain::TraceBack() {
      * 計算された操作量を用いて微分制御を行う（D制御）
      */
     unsigned long delta_time = millis() - last_time_called_;
-    float deviation_differential = (float)(deviation - prev_deviation_) / (float)delta_time;
+    float deviation_differential = (float) (deviation - prev_deviation_) / (float) delta_time;
 
     /**
      * 操作量の計算（PIDの各重みを考慮して最終的な操作量を計算する）
      */
-    int manipulation = (int)(p_ * deviation + d_ * deviation_differential);
+    int manipulation = (int) (p_ * deviation + d_ * deviation_differential);
 
     // 操作量の範囲を制限
     manipulation = constrain(manipulation, -max_manipulation_, max_manipulation_);
@@ -110,12 +134,4 @@ Instruction *PidLineTraceBrain::TraceBack() {
 Instruction *PidLineTraceBrain::Finish() {
     // do nothing...
     return new WaitInstruction(10);
-}
-
-PidLineTraceBrain::PidLineTraceBrain(int base_speed, float p, float d, int lr_sensor_diff, int max_manipulation) {
-    this->base_speed_ = base_speed;
-    this->p_ = p;
-    this->d_ = d;
-    this->lr_sensor_diff_ = lr_sensor_diff;
-    this->max_manipulation_ = max_manipulation;
 }
--- lib/car_control/custom/brain/pid_line_trace_brain.h
+++ lib/car_control/custom/brain/pid_line_trace_brain.h
@@ -2,17 +2,20 @@
 #define LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_PID_LINE_TRACE_BRAIN_H
 
 #include "line_trace_brain.h"
+#include "custom/data_model/trace_mode.h"
 
 class PidLineTraceBrain : public LineTraceBrain {
 public:
     /**
+     * @param trace_mode トレースモード（行動パターンを決定）
      * @param base_speed 基本速度
+     * @param turn_speed Uターン時の回転速度
      * @param p p制御の重み（比例制御 普通の調整）
      * @param d d制御の重み（微分制御 急なカーブへの対応など）
      * @param lr_sensor_diff 望ましい位置にあるときの(左のセンサ値 - 右のセンサ値)の値。個体値を吸収する役割。
      * @param max_manipulation 最大の速度操作量（制限）
      */
-    PidLineTraceBrain(int base_speed, float p, float d, int lr_sensor_diff, int max_manipulation);
+    PidLineTraceBrain(TraceMode trace_mode, int base_speed, int turn_speed, float p, float d, int lr_sensor_diff, int max_manipulation);
 
 protected:
     Instruction *Ready() override;
@@ -30,7 +33,9 @@ protected:
     Instruction *Finish() override;
 
 private:
+    TraceMode trace_mode_;
     int base_speed_;
+    int turn_speed_;
     float p_;
     float d_;
     int lr_sensor_diff_;
