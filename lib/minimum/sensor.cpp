8514192bf22b66cb3a0ebc8bdd52025b69f2c108
Fujita723 <115635408+Fujita723@users.noreply.github.com>
2022-12-08 16:24:23 +0900

    Sensorクラスの作成

--- lib/minimum/sensor.cpp
+++ lib/minimum/sensor.cpp
@@ -1,3 +1,21 @@
 #include "sensor.h"
+#include <Arduino.h>
 
 /* TODO: ここに各メソッドを実装する */
+
+// センサーの初期化
+Sensor::Sensor(int pin) {
+    this->pin_ = pin;
+    this->value_ = 0;
+    pinMode(this->pin_, INPUT);
+}
+
+// センサーの更新
+void Sensor::Update() {
+    this->value_ = analogRead(this->pin_);
+}
+
+// センサーの現在の値を取得
+int Sensor::GetRawValue() {
+    return this->value_;
+}
