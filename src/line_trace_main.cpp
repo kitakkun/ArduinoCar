336f9cf01f46af081d462e50105e3fcacbe54cee
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-10 08:53:21 +0900

    LineTraceCar→TraceCarへの命名変更対応

--- src/line_trace_main.cpp
+++ src/line_trace_main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
-#include "line_trace_car.h"
+#include "trace_car.h"
 
-LineTraceCar *car;
+TraceCar *car;
 
 const int delay_millis = 10;
 const int reflector_theta = 500;
 
 void setup() {
-    car = new LineTraceCar(
+    car = new TraceCar(
             new Motor(2, 3, 10),
             new Motor(4, 5, 11),
             new PhotoReflector(A0, reflector_theta),
