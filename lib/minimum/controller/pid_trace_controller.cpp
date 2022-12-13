9200159d47907530ac58e4b35b85e55fa0d08487
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-13 17:18:01 +0900

    コメント追加

--- lib/minimum/controller/pid_trace_controller.cpp
+++ lib/minimum/controller/pid_trace_controller.cpp
@@ -22,20 +22,35 @@ void PidTraceController::Operate() {
 }
 
 void PidTraceController::Trace() {
+    /**
+     * deviation > 0 => 左の方が黒に近付いている
+     * deviation < 0 => 右の方が黒に近付いている
+     * この差分を用いて単純な比例制御を行う（P制御）
+     */
     int deviation = this->car_->GetLeftReflector()->GetRawValue()
                     - this->car_->GetRightReflector()->GetRawValue()
                     - this->lr_sensor_diff_;
 
+    /**
+     * 前に計算されたprev_deviation_と今回計算したdeviation、経過時間delta_timeを用いてdeviationの微分(deviation_differential)を計算する。
+     * 計算された操作量を用いて微分制御を行う（D制御）
+     */
     unsigned long delta_time = millis() - last_time_called_;
     float deviation_differential = (float) (deviation - this->prev_deviation_) / (float) delta_time;
 
+    /**
+     * 操作量の計算（PIDの各重みを考慮して最終的な操作量を計算する）
+     */
     int manipulation = (int) (this->p_weight_ * deviation + d_weight_ * deviation_differential);
 
+    // 操作量の範囲を制限
     manipulation = constrain(manipulation, -this->max_manipulation_, this->max_manipulation_);
 
+    // deviationの上書きと時間更新（次回の準備）
     this->prev_deviation_ = deviation;
     this->last_time_called_ = millis();
 
+    // 反映
     this->car_->GetLeftMotor()->UpdateSpeed(this->base_speed_ - manipulation);
     this->car_->GetRightMotor()->UpdateSpeed(this->base_speed_ + manipulation);
 }
