c6cb722ed818c7a1dec87b5a7b3368816aa16d41
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-15 23:05:15 +0900

    fake.hの追加
    FakeComponentsインクルード用

---
+++ lib/minimum/fake.h
@@ -0,0 +1,8 @@
+#ifndef LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_FAKE_H
+#define LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_FAKE_H
+
+#include "fake/fake_reflector.h"
+#include "fake/fake_sensor.h"
+#include "fake/fake_motor.h"
+
+#endif //LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_FAKE_H
