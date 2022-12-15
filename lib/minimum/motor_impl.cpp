06a6dd8e00f1e2542b83f5cf2c0dee91c9b4ae9e
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-15 17:41:20 +0900

    Motorクラスをインタフェースと実装に分離

--- lib/minimum/motor_impl.cpp
+++ lib/minimum/motor_impl.cpp
@@ -1,8 +1,7 @@
-#include "motor.h"
+#include "motor_impl.h"
 #include <Arduino.h>
 
-/* TODO: 各メソッドの実装 */
-Motor::Motor(int plus_pin, int minus_pin, int pwm_pin, int gain){
+MotorImpl::MotorImpl(int plus_pin, int minus_pin, int pwm_pin, int gain) {
     this->plus_pin_ = plus_pin;
     this->minus_pin_ = minus_pin;
     this->pwm_pin_ = pwm_pin;
@@ -14,30 +13,30 @@ Motor::Motor(int plus_pin, int minus_pin, int pwm_pin, int gain){
     pinMode(this->pwm_pin_, OUTPUT);
 }
 
-MoveDirection Motor::GetMoveDirection(){
+MoveDirection MotorImpl::GetMoveDirection() {
     return this->direction_;
 }
 
-int Motor::GetSpeed(){
+int MotorImpl::GetSpeed() {
     return this->speed_;
 }
 
-void Motor::UpdateSpeed(int speed){
+void MotorImpl::UpdateSpeed(int speed) {
     this->speed_ = speed;
     ApplySpeed();
 }
 
-void Motor::UpdateDirection(MoveDirection direction){
+void MotorImpl::UpdateDirection(MoveDirection direction) {
     this->direction_ = direction;
     ApplyDirection();
 }
 
-void Motor::ApplySpeed() {
-    int speed=constrain(this->speed_ + this->gain_, 0, 255);
+void MotorImpl::ApplySpeed() {
+    int speed = constrain(this->speed_ + this->gain_, 0, 255);
     analogWrite(this->pwm_pin_, speed);
 }
 
-void Motor::ApplyDirection() {
+void MotorImpl::ApplyDirection() {
     if (this->direction_ == forward) {
         digitalWrite(this->plus_pin_, HIGH);
         digitalWrite(this->minus_pin_, LOW);
