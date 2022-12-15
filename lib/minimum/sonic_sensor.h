b474d4217ba624d60252f7411a321c2d073298bb
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-15 15:24:28 +0900

    軽微な修正

--- lib/minimum/sonic_sensor.h
+++ lib/minimum/sonic_sensor.h
@@ -1,20 +1,21 @@
 #ifndef LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_SONIC_SENSOR_H
 #define LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_SONIC_SENSOR_H
 
+#include <AceRoutine.h>
 
 class SonicSensor : ace_routine::Coroutine {
 public:
     explicit SonicSensor(int trig_pin, int echo_pin);
 
-    int Update() override;
+    int Update();
 
     float GetRawValue();
 
-    private:
+private:
     int trig_pin_;
     int echo_pin_;
     float raw_value_;
-}
+};
 
 #endif //LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_SONIC_SENSOR_H
  
