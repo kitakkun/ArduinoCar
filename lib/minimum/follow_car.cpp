9217cd553714a93335a9bbf420d0db01d206f3c7
unchang <sagumemurata31@gmail.com>
2022-12-15 16:09:50 +0900

    SensorクラスをSOnicSensorクラスに置き換え

--- lib/minimum/follow_car.cpp
+++ lib/minimum/follow_car.cpp
@@ -1,12 +1,11 @@
 #include "follow_car.h"
 
-/* TODO: 実装 */
 
 FollowCar::FollowCar(
     Motor *left_motor,
     Motor *right_motor,
-    Sensor *left_sensor,
-    Sensor *right_sensor
+    SonicSensor *left_sensor,
+    SonicSensor *right_sensor
 ) {
     this->left_motor_ = left_motor;
     this->right_motor_ = right_motor;
@@ -22,10 +21,10 @@ Motor *FollowCar::GetRightMotor() {
     return this->right_motor_;
 }
 
-Sensor *FollowCar::GetLeftSensor() {
+SonicSensor *FollowCar::GetLeftSensor() {
     return this->left_sensor_;
 }
 
-Sensor *FollowCar::GetRightSensor() {
+SonicSensor *FollowCar::GetRightSensor() {
     return this->right_sensor_;
 }
