4671a5f8258daa137d8b35c5773bda1532228d93
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-22 21:37:43 +0900

    テストコード追加

---
+++ test/test_heartbeat_led/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "impl/heartbeat_led_impl.h"
+#include "unity.h"
+
+/**
+ * ハートビートLEDの点滅テスト
+ * 1秒間に1回間隔でLEDをオンオフする
+ * 10秒間テスト
+ */
+
+void setup() {
+    HeartbeatLedImpl *heartbeatLed = new HeartbeatLedImpl(13, 1000);
+    delay(1000);
+    UNITY_BEGIN();
+    unsigned long start_time = millis();
+    // 5秒間点滅させる
+    while (millis() - start_time < 10000) {
+        heartbeatLed->runCoroutine();
+    }
+    UNITY_END();
+}
+
+void loop() {}
