5104482a7e841e702e7f6e92b66130156a839d3e
kamisyusyu <116176179+kamisyusyu@users.noreply.github.com>
2022-12-22 17:22:53 +0900

    follow車のパラメータ調整

--- src/follow_main.cpp
+++ src/follow_main.cpp
@@ -4,9 +4,10 @@
 #include "controller/pid_follow_controller.h"
 #include "ArduinoLog.h"
 #include "impl.h"
+#include "debug/follow_car_debugger.h"
 
 CarController *controller;
-
+FollowCarDebugger *debugger;
 const int delay_millis = 10;
 
 void setup() {
@@ -14,27 +15,29 @@ void setup() {
     while (!Serial);
     Log.begin(LOG_LEVEL_VERBOSE, &Serial, true);
     Log.verboseln("Building a Car instance...");
-    auto *car = new FollowCar(
-        new MotorImpl(2, 3, 10),
-        new MotorImpl(4, 5, 11),
-        new SonicSensorImpl(A0, A0),    // TODO:pin番号を入力する
-        new SonicSensorImpl(A0, A0)
+    FollowCar *car = new FollowCar(
+        new MotorImpl(2, 3, 11),
+        new MotorImpl(4, 5, 10),
+        new SonicSensorImpl(6, 7),    // TODO:pin番号を入力する
+        new SonicSensorImpl(9, 8)
     );
+
+    debugger = new FollowCarDebugger(car);
 
     car->GetLeftMotor()->UpdateDirection(forward);
     car->GetRightMotor()->UpdateDirection(forward);
 
     controller = new PidFollowController(
         car,
-        5,
-        100,
-        -4,
-        80,
+        7.5,
+        105,
+        0,
         20,
-        0.052,
-        0.05,
-        0.042,
-        0.05
+        50,
+        0.50,
+        0.50,
+        10.0,
+        6.0
     );
 }
 
@@ -42,4 +45,7 @@ void loop() {
     controller->Update();
     controller->Operate();
     delay(delay_millis);
+    debugger->DebugSensors();
+    //Serial.println(car->GetLeftSensor()->GetRawValue());
+    // Serial.println(car->GetLeftSensor()->GetRawValue());
 }
