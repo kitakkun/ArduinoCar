c3675ca15f9346217a86bb61dfec9035af046889
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-12-22 14:53:22 +0900

    FollowCarBuilderの定義

---
+++ lib/minimum/carbuilder/follow_car_builder.h
@@ -0,0 +1,31 @@
+#ifndef LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_FOLLOW_CAR_BUILDER_H
+#define LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_FOLLOW_CAR_BUILDER_H
+
+
+#include "interface/sonic_sensor.h"
+#include "interface/motor.h"
+#include "car/follow_car.h"
+
+class FollowCarBuilder {
+public:
+    explicit FollowCarBuilder();
+
+    void SetLeftMotor(Motor *left_motor);
+
+    void SetRightMotor(Motor *left_motor);
+
+    void SetLeftSensor(SonicSensor *left_sensor);
+
+    void SetRightSensor(SonicSensor *right_sensor);
+
+    FollowCar *Build();
+
+private:
+    Motor *left_motor_;
+    Motor *right_motor_;
+    SonicSensor *left_sensor_;
+    SonicSensor *right_senor_;
+};
+
+
+#endif //LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_FOLLOW_CAR_BUILDER_H
