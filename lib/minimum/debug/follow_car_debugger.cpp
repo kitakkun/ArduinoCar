91f36a211058d41e144d0f427a486ec0a472ed47
kamisyusyu <116176179+kamisyusyu@users.noreply.github.com>
2022-12-22 16:49:41 +0900

    Revert "出力フォーマット調整"
    This reverts commit cb9662d6c856206896b6900e1e25e7760e681e77.

--- lib/minimum/debug/follow_car_debugger.cpp
+++ lib/minimum/debug/follow_car_debugger.cpp
@@ -1,5 +1,4 @@
 #include "follow_car_debugger.h"
-#include "ArduinoLog.h"
 #include <Arduino.h>
 
 FollowCarDebugger::FollowCarDebugger(FollowCar *follow_car) {
@@ -8,18 +7,16 @@ FollowCarDebugger::FollowCarDebugger(FollowCar *follow_car) {
 
 //センサーの出力
 void FollowCarDebugger::DebugSensors() {
-    Log.verboseln(
-        "LeftSensor: %d, RightSensor: %d",
-        this->follow_car_->GetLeftSensor()->GetRawValue(),
-        this->follow_car_->GetRightSensor()->GetRawValue()
-    );
+    Serial.print(" LeftSensor= ");
+    Serial.print(this->follow_car_->GetLeftSensor()->GetRawValue());
+    Serial.print(" RightSensor= ");
+    Serial.println(this->follow_car_->GetRightSensor()->GetRawValue());
 }
 
 //モーターの出力
 void FollowCarDebugger::DebugMotors() {
-    Log.verboseln(
-        "LeftSpeed: %d, RightSpeed: %d",
-        this->follow_car_->GetLeftMotor()->GetSpeed(),
-        this->follow_car_->GetRightMotor()->GetSpeed()
-    );
+    Serial.print(" LeftMotor= ");
+    Serial.print(this->follow_car_->GetLeftMotor()->GetSpeed());
+    Serial.print(" RightMotor= ");
+    Serial.println(this->follow_car_->GetRightMotor()->GetSpeed());
 }
