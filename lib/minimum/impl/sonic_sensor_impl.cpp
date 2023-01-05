eac1590903b6fdf5a084b015fdc521cc134fef71
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-01-05 15:08:56 +0900

    超音波センサーが最終更新から60ms経っていないと更新しないようにする

--- lib/minimum/impl/sonic_sensor_impl.cpp
+++ lib/minimum/impl/sonic_sensor_impl.cpp
@@ -4,6 +4,7 @@
 SonicSensorImpl::SonicSensorImpl(int trig_pin, int echo_pin) {
     this->trig_pin_ = trig_pin;
     this->echo_pin_ = echo_pin;
+    this->last_updated_time_ = millis();
     pinMode(this->trig_pin_, OUTPUT);
     pinMode(this->echo_pin_, INPUT);
 }
@@ -28,6 +29,12 @@ double SonicSensorImpl::GetRawValue() {
 int SonicSensorImpl::runCoroutine() {
     unsigned long duration;
     COROUTINE_BEGIN();
+
+    // 最終更新から60ms経過するまで次の更新タスクを実行しない
+    while(millis() - last_updated_time_ < 60) {
+        COROUTINE_YIELD();
+    }
+
     //念のために最初に超音波を止める
     digitalWrite(this->trig_pin_, LOW);
     delayMicroseconds(2);
@@ -41,5 +48,7 @@ int SonicSensorImpl::runCoroutine() {
 
     //距離の計算
     this->raw_value_ = (double) duration * 340.0 / 10000.0;
+
+    this->last_updated_time_ = millis();
     COROUTINE_END();
 }
