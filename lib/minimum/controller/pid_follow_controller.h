d17743a4d2a0491d91afe61f9d3aa4717ef8368a
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-01-05 16:13:32 +0900

    base_distanceをfloat型に変える

--- lib/minimum/controller/pid_follow_controller.h
+++ lib/minimum/controller/pid_follow_controller.h
@@ -10,7 +10,7 @@ class PidFollowController : public CarController {
 public:
     explicit PidFollowController(
         FollowCar *car,
-        int base_distance,
+        float base_distance,
         int base_speed,
         int ir_sensor_diff,
         int max_manipulation_dist,
@@ -28,7 +28,7 @@ public:
 protected:
     FollowCar *car_;
     SonicSensorUpdater *sensor_updater_;
-    int base_distance_;
+    float base_distance_;
     int base_speed_;
     int lr_sensor_diff_; // 左右のセンサの個体差
     int max_manipulation_dist_;
