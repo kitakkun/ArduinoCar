b53fb3c95d12eb5e710f8fef81f76b56c3568507
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-01-12 15:15:18 +0900

    MotorImplをimpl/motorへ移動

--- lib/minimum/impl.h
+++ lib/minimum/impl.h
@@ -1,7 +1,7 @@
 #ifndef LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_IMPL_H
 #define LABORATORY_WORK_IN_INFORMATION_ENGINEERING_III_IMPL_H
 
-#include "impl/motor_impl.h"
+#include "impl/motor/motor_impl.h"
 #include "impl/sensor/analog_sensor_impl.h"
 #include "impl/sensor/photo_reflector_impl.h"
 #include "impl/sensor/sonic_sensor_impl.h"
